4e7d5c01c6af12d242e2215ee2cc2675bdff14c0
aarjaw <aarjawadhikari12@gmail.com>
2024-01-28 15:35:39 +0545

    first commit

---
+++ src/script.js
@@ -0,0 +1,66 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed",
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+const time = gsap.timeline();
+time.from(".portfolio, .contact", {
+  y: -100,
+  duration: 2,
+});
+
+time.from(".left .hello", {
+  y: -250,
+  duration: 2,
+});
+
+time.from(".left .name", {
+  x: -550,
+  duration: 2,
+});
+
+time.from(".buttons", {
+  x: 1500,
+  duration: 4,
+  opacity: 0,
+});
+
+time.from(".photo", {
+  y: 300,
+  duration: 2,
+  scale: 0,
+  opacity: 0,
+});
